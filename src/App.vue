a42f5ed9b718bfcee3155ce61e9f295d452decaf
Coma <coma@wonergames.es>
2020-05-28 12:19:26 +0200

    Jumbo draft
    Header with image and placeholder text

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
       <el-header>
         <div class="nav-bar">  
           <div class="nav-bar-title">Wondergames</div>
-          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="1">
-              <router-link to="/">
+          <el-menu :default-active="activeIndex" class="el-menu-demo" router mode="horizontal" @select="handleSelect">
+            <el-menu-item index="/">
                 Home
-              </router-link>
             </el-menu-item>
-            
+            <el-menu-item index="/community">
+                Comunidad
+            </el-menu-item>
+            <el-menu-item index="3">
+              <img src="./assets/logo.png" />
+            </el-menu-item>
           </el-menu>
         </div>
 
@@ -25,16 +28,39 @@
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data() {
+      return {
+          activeIndex: '1'
+      };
+    },
+  methods: {
+      handleSelect(key, keyPath) {
+          console.log(key, keyPath);
+          router.push('home')
+      }
+  }
+}
+</script>
+
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap');
 html {
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
 .nav-bar {
   display: flex;
-}
-.nav-bar-title {
-  font-family: 'Pacifico', cursive;
-  font-size: 32px;
+  border-bottom: solid 1px #e6e6e6;
+  a {
+    text-decoration: none;
+  }
+  .nav-bar-title {
+    font-family: 'Pacifico', cursive;
+    font-size: 28px;
+  }
+  .el-menu {
+    border-bottom: none;
+  }
 }
 </style>
